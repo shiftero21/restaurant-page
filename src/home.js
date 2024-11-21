640f9deecc9f51e30c92dcc5442a9f3ec6825031
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2024-11-21 10:34:13 -0300

    add basic text tab pages

--- src/home.js
+++ src/home.js
@@ -2,4 +2,10 @@ export function homeTab () {
   const homePage = document.createElement('div');
   homePage.id = 'home';
   document.getElementById('content').appendChild(homePage);
+  const homeTitle = document.createElement('h1');
+  homeTitle.textContent = 'HOME';
+  homePage.appendChild(homeTitle);
+  const homeDescription = document.createElement('p');
+  homeDescription.textContent = '¡Descubre el sabor que transforma cada comida en una experiencia inolvidable! En [Nombre del Restaurante], combinamos ingredientes frescos, recetas únicas y un ambiente acogedor para ofrecerte lo mejor de la gastronomía.';
+  homePage.appendChild(homeDescription);
 }
