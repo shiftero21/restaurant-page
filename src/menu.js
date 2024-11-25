c311e3daa9a3cb4b5b19f64054e39584e74b73fd
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2024-11-25 11:42:47 -0300

    add img for plates

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,8 @@
+import pastaPesto from './plates/pasta-al-pesto.jpg';
+import salmon from './plates/salmon-parilla.jpg';
+import risotto from './plates/risotto-hongos.jpg';
+import ribs from './plates/costillas-bbq.jpg';
+
 export function menuTab () {
   const menuPage = document.createElement('div');
   menuPage.id = 'menu';
@@ -17,6 +22,7 @@ export function menuTab () {
 
   const plate1 = document.createElement('div');
   plate1.innerHTML = `
+    <img src="${pastaPesto}" alt="Pasta al Pesto">
     <h3>Pasta al Pesto</h3>
     <p>Linguini artesanal con salsa pesto fresca, piñones tostados y parmesano añejo</p>
     <p class="price">$18.99</p>
@@ -24,6 +30,7 @@ export function menuTab () {
 
   const plate2 = document.createElement('div');
   plate2.innerHTML = `
+    <img src="${salmon}" alt="Salmón a la Parrilla">
     <h3>Salmón a la Parrilla</h3>
     <p>Filete de salmón fresco con salsa de cítricos, servido con vegetales de temporada</p>
     <p class="price">$24.99</p>
@@ -31,6 +38,7 @@ export function menuTab () {
 
   const plate3 = document.createElement('div');
   plate3.innerHTML = `
+    <img src="${risotto}" alt="Risotto de Hongos">
     <h3>Risotto de Hongos</h3>
     <p>Arroz arborio cremoso con variedad de hongos silvestres y trufa negra</p>
     <p class="price">$21.99</p>
@@ -38,6 +46,7 @@ export function menuTab () {
 
   const plate4 = document.createElement('div');
   plate4.innerHTML = `
+    <img src="${ribs}" alt="Costillas BBQ">
     <h3>Costillas BBQ</h3>
     <p>Costillas de cerdo cocidas a fuego lento con salsa barbacoa casera y puré de papas</p>
     <p class="price">$26.99</p>
